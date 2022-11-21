ef36290c99b133dbb3ea88e718244a3f653683cb
Blue-Mystical <74407949+Blue-Mystical@users.noreply.github.com>
2022-11-21 15:43:16 +0700

    cross validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from imutils import paths
 
@@ -32,6 +33,9 @@ force = args["forceresize"]
 generatesize = args["generatesize"]
 jobs = args["threads"]
 
+if generatesize >= 0 and force == False:
+	print("[WARN] the 'generatesize' argument requires 'forceresize' argmument to be True")
+
 # get the list of images
 print("[INFO] describing images...")
 data = LoadDataset()
@@ -46,15 +50,25 @@ rawImages, features, labels = data.load(pathes, verbose = 5, forceResize = force
 
 # TODO: THRESHOLDING WITH OTSU'S ALGORITHM
 
+
 # train test
+# , random_state=4
 (trainImg, testImg, trainImgLabel, testImgLabel) = train_test_split(
-	rawImages, labels, test_size = train_test_split_size, random_state=31)
+	rawImages, labels, test_size = train_test_split_size)
 (trainFeature, testFeature, trainFeatureLabels, testFeatureLabels) = train_test_split(
-	features, labels, test_size = train_test_split_size, random_state=31)
+	features, labels, test_size = train_test_split_size)
+
+# TODO: 
 
 print("[INFO] calculating knn algorithm...")
 print("[INFO] train-test split ratio: {:.0f}/{:.0f}".format((1 - train_test_split_size) * 100, train_test_split_size * 100))
 model = KNeighborsClassifier(n_neighbors = k, n_jobs=jobs)
-model.fit(trainImg, trainImgLabel)
-accuracy = model.score(testImg, testImgLabel)
-print("[INFO] raw accuracy: {:.2f}%".format(accuracy * 100))
+# model.fit(trainImg, trainImgLabel)
+
+# accuracy = model.score(testImg, testImgLabel)
+# print("[INFO] raw accuracy: {:.2f}%".format(accuracy * 100))
+
+cv_scores = cross_val_score(model, trainImg, trainImgLabel, cv = 5)
+print("[INFO] cross validation accuracy across 5 predictions: ")
+print(cv_scores)
+print("[INFO] average accuracy: {}".format(np.mean(cv_scores)))
